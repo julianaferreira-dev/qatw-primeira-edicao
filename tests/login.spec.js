5ef63eaae590d81ebdd2f31d32d006a89f3c9013
Juliana Ferreira <julianaferreira.sants@gmail.com>
2025-03-04 20:49:16 -0300

    pipeline de testes

---
+++ tests/login.spec.js
@@ -0,0 +1,50 @@
+import { test, expect } from '@playwright/test';
+import { obterCodigo2FA } from '../support/db';
+import { LoginPage } from '../page/LoginPagina';
+import { DashPage } from '../page/DashPage';
+import { cleanJobs, getJob } from '../support/redis';
+
+test('Não deve logar quando o código de autenticação for inválido', async ({ page }) => {
+  const loginPage = new LoginPage(page);
+
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258'
+  }
+
+  await loginPage.acessaPagina();
+  await loginPage.informaCpf(usuario.cpf);
+  await loginPage.informaSenha(usuario.senha);
+  await loginPage.informa2FA('123456');
+
+  expect(await page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
+});
+
+test('Deve acessar a conta do usuário', async ({ page }) => {
+
+  const loginPage = new LoginPage(page);
+  const dashPage = new DashPage(page);
+
+  const usuario = {
+    cpf: '00000014141',
+    senha: '147258'
+  }
+
+  await cleanJobs();
+
+  await loginPage.acessaPagina();
+  await loginPage.informaCpf(usuario.cpf);
+  await loginPage.informaSenha(usuario.senha);
+
+  //checkpoint
+  await page.getByRole('heading', { name: 'Verificação em duas etapas'})
+    .waitFor({ timeout: 3000 });
+
+  const codigo = await getJob()
+
+  // const codigo = await obterCodigo2FA(usuario.cpf);
+
+  await loginPage.informa2FA(codigo);
+
+  await expect(await dashPage.obterSaldo()).toHaveText('R$ 5.000,00');
+});
